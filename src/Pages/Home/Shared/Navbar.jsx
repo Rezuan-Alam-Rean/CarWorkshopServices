8df6b8a30b8880654783112925b29f2af404d9bb
Rezuan-Alam-Rean <rezuanrean.cse@gmail.com>
2024-01-10 06:22:03 +0600

    update on navbar

--- src/Pages/Home/Shared/Navbar.jsx
+++ src/Pages/Home/Shared/Navbar.jsx
@@ -1,4 +1,7 @@
+import { Link } from "react-router-dom";
 import img from "../../../assets/assets/logo.svg"
+import { CiSearch } from "react-icons/ci";
+import { HiOutlineShoppingBag } from "react-icons/hi2";
 
 
 const Navbar = () => {
@@ -10,36 +13,32 @@ const Navbar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <a>Parent</a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+
+                        <li><Link>Home</Link></li>
+
+                        <li><Link>About</Link></li>
+                        <li><Link>Services</Link></li>
+                        <li><Link>Blog</Link></li>
+                        <li><Link>Contact</Link></li>
                     </ul>
                 </div>
                 <img src={img} alt="logo" />
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
-                    <li>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li><a>Item 3</a></li>
+                    <li><Link>Home</Link></li>
+
+                    <li><Link>About</Link></li>
+                    <li><Link>Services</Link></li>
+                    <li><Link>Blog</Link></li>
+                    <li><Link>Contact</Link></li>
                 </ul>
             </div>
             <div className="navbar-end">
-                <a className="btn">Button</a>
+            <HiOutlineShoppingBag className="w-6 h-6 mr-4" />
+
+            <CiSearch className="w-6 h-6 mr-4" />
+                <a className="btn btn-outline btn-error">Appointment</a>
             </div>
         </div>
     );
