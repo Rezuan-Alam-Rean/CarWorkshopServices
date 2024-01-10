000e7777ead1bb615476fbd38d67cc25637c2d6f
Rezuan-Alam-Rean <rezuanrean.cse@gmail.com>
2024-01-11 05:47:42 +0600

    create banner

--- src/Pages/Home/Shared/Navbar.jsx
+++ src/Pages/Home/Shared/Navbar.jsx
@@ -5,6 +5,18 @@ import { HiOutlineShoppingBag } from "react-icons/hi2";
 
 
 const Navbar = () => {
+
+    const navItems = <>
+
+
+        <li><Link>Home</Link></li>
+        <li><Link>About</Link></li>
+        <li><Link>Services</Link></li>
+        <li><Link>Blog</Link></li>
+        <li><Link>Contact</Link></li>
+
+    </>
+
     return (
         <div className="navbar  shadow-xl text-black bg-white">
             <div className="navbar-start">
@@ -13,31 +25,20 @@ const Navbar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-
-                        <li><Link>Home</Link></li>
-
-                        <li><Link>About</Link></li>
-                        <li><Link>Services</Link></li>
-                        <li><Link>Blog</Link></li>
-                        <li><Link>Contact</Link></li>
+                        {navItems}
                     </ul>
                 </div>
-                <img src={img} alt="logo" />
+                <img className="h-16 w-h-16" src={img} alt="logo" />
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><Link>Home</Link></li>
-
-                    <li><Link>About</Link></li>
-                    <li><Link>Services</Link></li>
-                    <li><Link>Blog</Link></li>
-                    <li><Link>Contact</Link></li>
+                    {navItems}
                 </ul>
             </div>
             <div className="navbar-end">
-            <HiOutlineShoppingBag className="w-6 h-6 mr-4" />
+                <HiOutlineShoppingBag className="w-6 h-6 mr-4" />
 
-            <CiSearch className="w-6 h-6 mr-4" />
+                <CiSearch className="w-6 h-6 mr-4" />
                 <a className="btn btn-outline btn-error">Appointment</a>
             </div>
         </div>
