bc8b97ad21d2d003dbb96dad6cfafef7f0ab2d41
Rezuan-Alam-Rean <rezuanrean.cse@gmail.com>
2024-03-13 20:28:42 +0600

    update

--- src/Pages/Home/Shared/Navbar.jsx
+++ src/Pages/Home/Shared/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
 
         <li><Link>Home</Link></li>
         <li><Link to="about">About</Link></li>
-        <li><Link>Services</Link></li>
+        <li><Link to="Services">Services</Link></li>
         <li><Link>Blog</Link></li>
         <li><Link>Contact</Link></li>
 
--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home";
 import About from "../Pages/Home/About/About";
+import Services from "../Pages/Home/Services/Services";
 
 
 const router = createBrowserRouter([
@@ -12,6 +13,11 @@ const router = createBrowserRouter([
         {
             path: "/",
             element: <Home/>,
+
+        },
+        {
+            path: "/Services",
+            element: <Services/>,
 
         },
 
